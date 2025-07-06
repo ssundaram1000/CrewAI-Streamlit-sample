b5c031e8ca397c06fca5f6129d78e6bed136fb90
Charu Shridhar <shridhar@backbase.com>
2025-07-06 07:23:30 -0400

    try1

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,8 +3,8 @@ from pathlib import Path
 
 import sys
 try:
-    import pysqlite3                      # present on Linux build images
-    sys.modules["sqlite3"] = sys.modules.pop("pysqlite3")
+    __import__('pysqlite3')
+    sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
 except ModuleNotFoundError:
     # We're on macOS (no wheel) or the wheel failed to install.
     # Either fall back to the real sqlite3, or leave your stub in place.
